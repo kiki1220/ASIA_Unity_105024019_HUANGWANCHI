95de46ca1916465bdf49484745b4b5840aa1386e
kiki1220 <55470495+kiki1220@users.noreply.github.com>
2020-01-07 15:19:09 +0800

    ok

---
+++ Unity_105024019_HUANGWANCHI/Assets/script/EatProp.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using UnityEngine.UI;               // 引用 介面 API
+using UnityEngine.SceneManagement;  // 引用 場景管理 API
+
+public class EatProp : MonoBehaviour
+{
+    [Header("錢的數量介面")]
+    public Text textCount;
+    [Header("提示畫面")]
+    public GameObject objTip;
+
+    public int propCount;       // 目前寶箱數量
+    public int propTotal = 10;  // 寶箱總數
+
+    public bool finish;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "錢")
+        {
+            Destroy(collision.gameObject);
+
+            propCount++;                                            // 遞增
+            textCount.text = "錢：" + propCount + " / 10";        // 更新介面
+
+            // 如果 目前數量 等於 總數 顯示 勝利提示
+            if (propCount == propTotal)
+            {
+                finish = true;
+                objTip.SetActive(true);
+            }
+        }
+    }
+
+    private void Update()
+    {
+        GameOver();
+    }
+
+    private void GameOver()
+    {
+        if (finish)
+        {
+            // 按下 ESC 離開
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                Application.Quit();     // 應用程式.離開();
+            }
+            // 按下 R 重來
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                //Application.LoadLevel("遊戲場景");    // 舊版 API
+                SceneManager.LoadScene("SampleScene");     // 新版 API
+            }
+        }
+    }
+}
---
+++ Unity_105024019_HUANGWANCHI/Assets/script/player.cs
@@ -0,0 +1,55 @@
+﻿
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    #region 欄位區域
+    [Header("動畫參數名稱")]
+    public string parRun = "跑步開關";
+    [Header("道具")]
+    public int countNeed = 10;
+    public int countCurrent;
+    public Animator ani;
+    #endregion
+
+    #region 方法區域
+    /// <summary>
+    /// 跑步
+    /// </summary>
+    private void Run()
+    {
+        //print("水平 " + Input.GetAxis("Horizontal")); // Horizontal A 左 -1、D 右 1、沒按 0
+        //print("垂直 " + Input.GetAxis("Vertical"));   // Vertical   S 下 -1、W 上 1、沒按 0
+
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        ani.SetBool("跑步開關", h != 0 || v != 0);  // 水平 不等於 0 或者 垂直 不等於 0 就跑步
+    }
+
+    
+
+   
+    
+    /// <summary>
+    /// 吃道具
+    /// </summary>
+    private void EatProp()
+    {
+
+    }
+
+    /// <summary>
+    /// 過關
+    /// </summary>
+    private void Pass()
+    {
+
+    }
+    #endregion
+
+    private void Update()
+    {
+        Run();
+    }
+}
